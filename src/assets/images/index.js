ab80fdd29d1e89f273351078f1407792bc98f7b0
Dawinz <71150010+Dawinz@users.noreply.github.com>
2024-01-15 08:33:54 +0300

    profile logo changes

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,4 @@
-import logo from "./computerbeipoa.png";
+import logo from "./narita.jpg";
 import logoLight from "./logoLight.png";
 import bannerImgOne from "./banner/bannerImgOne.webp";
 import bannerImgTwo from "./banner/bannerImgTwo.webp";
